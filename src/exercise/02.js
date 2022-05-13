a5a94088894722cafe4474925afec092a653e130
coofzilla <jeric614@yahoo.com>
2022-05-13 16:11:21 +0900

    exercise 2 refactored w/custom hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,25 +3,31 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+const useLocalStorageState = (key, defaultValue = '') => {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
+  )
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [state, setState] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
-    setName(event.target.value)
+    setState(event.target.value)
   }
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={state} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {state ? <strong>Hello {state}</strong> : 'Please type your name'}
     </div>
   )
 }
