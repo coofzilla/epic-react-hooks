05223843343d77b76f5e5caf40c4475a98ce6ba1
coofzilla <jeric614@yahoo.com>
2022-06-13 11:15:13 +0900

    custom serializer

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,20 +3,35 @@
 
 import * as React from 'react'
 
-const useLocalStorageState = (key, defaultValue = '') => {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) ?? defaultValue,
-  )
+const useLocalStorageState = (
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return defaultValue
+  })
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
 
   return [state, setState]
 }
 
+const customSerializer = input => {
+  console.log('IM CUSTOM BABY')
+  return JSON.stringify(input)
+}
+
 function Greeting({initialName = ''}) {
-  const [state, setState] = useLocalStorageState('name', initialName)
+  const [state, setState] = useLocalStorageState('name', initialName, {
+    serialize: customSerializer,
+  })
 
   function handleChange(event) {
     setState(event.target.value)
