224eac17a1e9fbb7d6640d3553a8b8b59a92cf68
coofzilla <jeric614@yahoo.com>
2022-06-18 20:11:20 +0900

    define error boundary

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -8,8 +8,25 @@ import {
   PokemonForm,
   PokemonDataView,
   fetchPokemon,
-  PokemonErrorBoundary,
 } from '../pokemon'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false}
+  }
+  static getDerivedStateFromError(error) {
+    return {hasError: true}
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>
+    }
+
+    return this.props.children
+  }
+}
 
 function PokemonInfo({pokemonName}) {
   const [{status, pokemon, error}, setState] = React.useState({
