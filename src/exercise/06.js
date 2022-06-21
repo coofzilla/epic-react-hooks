51a56b40ce0d412f8e577c050e53bda5cd6ae956
coofzilla <jeric614@yahoo.com>
2022-06-22 00:35:28 +0900

    implement resetkeys

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -94,6 +94,7 @@ function App() {
         <ErrorBoundary
           FallbackComponent={ErrorFallback}
           onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
         >
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
